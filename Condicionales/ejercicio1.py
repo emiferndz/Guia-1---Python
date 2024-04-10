a543c3162aa67718dadc5ef3252157ee6254e8c7
Emi <m1h0r5h@gmail.com>
2024-04-09 21:11:22 -0300

    primer commit de la guia 1 python

---
+++ Condicionales/ejercicio1.py
@@ -0,0 +1,7 @@
+primer_num= int(input("Ingresa el primer numero: "))
+segundo_num= int(input("Ingrese el segundo numero: "))
+
+if primer_num == segundo_num:
+    print("Los numeros son iguales")
+else:
+    print("Los numeros son distintos")
---
+++ Secuenciales/ejercicio6.py
@@ -0,0 +1,8 @@
+precio = float(input("Ingrese el precio de lista del artículo: "))
+
+preciocontado = precio * 0.9
+
+preciotarjeta = precio * 1.05  
+
+print("El precio de venta al contado es:", preciocontado)
+print("El precio de venta con tarjeta es:", preciotarjeta)
